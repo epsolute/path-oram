423537b4ae04a621924b7b99d33785a49b511947
Dmytro Bogatov <dmytro@dbogatov.org>
2020-03-26 00:24:21 -0400

    Add first benchmark.
    Add storage adapter benchmark.
    Use both versions.
    Adapt makefile for benchmarks.
    Remove main file.
    Add benchmarks to CI.

---
+++ path-oram/benchmark/benchmark-storage-adapter.cpp
@@ -0,0 +1,99 @@
+#include "definitions.h"
+#include "storage-adapter.hpp"
+
+#include <benchmark/benchmark.h>
+#include <boost/format.hpp>
+#include <fstream>
+
+using namespace std;
+
+namespace PathORAM
+{
+	enum TestingStorageAdapterType
+	{
+		StorageAdapterTypeInMemory,
+		StorageAdapterTypeFileSystem
+	};
+
+	class StorageAdapterBenchmark : public ::benchmark::Fixture
+	{
+		public:
+		inline static const ulong CAPACITY   = 1 << 17;
+		inline static const ulong BLOCK_SIZE = 32;
+		inline static const string FILE_NAME = "storage.bin";
+
+		protected:
+		AbsStorageAdapter* adapter;
+
+		~StorageAdapterBenchmark() override
+		{
+			delete adapter;
+			remove(FILE_NAME.c_str());
+		}
+
+		void Configure(TestingStorageAdapterType type)
+		{
+			switch (type)
+			{
+				case StorageAdapterTypeInMemory:
+					adapter = new InMemoryStorageAdapter(CAPACITY, BLOCK_SIZE, bytes());
+					break;
+				case StorageAdapterTypeFileSystem:
+					adapter = new FileSystemStorageAdapter(CAPACITY, BLOCK_SIZE, bytes(), FILE_NAME, true);
+					break;
+				default:
+					throw boost::format("TestingStorageAdapterType %2% is not implemented") % type;
+			}
+		}
+	};
+
+	BENCHMARK_DEFINE_F(StorageAdapterBenchmark, Write)
+	(benchmark::State& state)
+	{
+		Configure((TestingStorageAdapterType)state.range(0));
+		auto random = state.range(1);
+
+		ulong location = 0;
+		for (auto _ : state)
+		{
+			adapter->set((location * (random ? (1 << 10) : 1)) % CAPACITY, {5uLL, bytes()});
+			location++;
+		}
+	}
+
+	BENCHMARK_DEFINE_F(StorageAdapterBenchmark, Read)
+	(benchmark::State& state)
+	{
+		Configure((TestingStorageAdapterType)state.range(0));
+		auto random = state.range(1);
+
+		for (ulong i = 0; i < CAPACITY; i++)
+		{
+			adapter->set(i, {5uLL, bytes()});
+		}
+
+		ulong location = 0;
+		for (auto _ : state)
+		{
+			benchmark::DoNotOptimize(adapter->get((location * (random ? (1 << 10) : 1)) % CAPACITY));
+		}
+	}
+
+	BENCHMARK_REGISTER_F(StorageAdapterBenchmark, Write)
+		->Args({StorageAdapterTypeInMemory, false})
+		->Args({StorageAdapterTypeInMemory, true})
+		->Args({StorageAdapterTypeFileSystem, false})
+		->Args({StorageAdapterTypeFileSystem, true})
+		->Iterations(1 << 15)
+		->Unit(benchmark::kMicrosecond);
+
+	BENCHMARK_REGISTER_F(StorageAdapterBenchmark, Read)
+		->Args({StorageAdapterTypeInMemory, false})
+		->Args({StorageAdapterTypeInMemory, true})
+		->Args({StorageAdapterTypeFileSystem, false})
+		->Args({StorageAdapterTypeFileSystem, true})
+		->Iterations(1 << 15)
+		->Unit(benchmark::kMicrosecond);
+}
+
+BENCHMARK_MAIN();
